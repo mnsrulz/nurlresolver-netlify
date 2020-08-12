844c8a280a94397592451106da99e1cd9cdd292d
Manish Rawat <manishrawat4u@gmail.com>
2020-08-12 12:23:41 -0400

    added nurlresolver

--- express/server.js
+++ express/server.js
@@ -4,18 +4,24 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
+const url = require('url');
 const router = express.Router();
+const nurlresolver = require('nurlresolver');
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
 
-router.get('/test', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello test!</h1>');
-  res.end();
+router.get('/resolve', async (req, res) => {
+  const { q } = url.parse(req.url, true).query;
+  if (q) {
+    const result = await nurlresolver.resolve(q);
+    res.json(result);
+  } else {
+    res.status(400).json({ error: 'Query param q not defined' });
+  }
 });
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
