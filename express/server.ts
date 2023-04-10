dc3c0ebeba9e8a15ada8e359058d07cc4fa2f1f3
mnsrulz <manishrawat4u@gmail.com>
2023-04-10 03:03:50 +0000

    another fix

--- express/server.ts
+++ express/server.ts
@@ -22,12 +22,13 @@ router.get('/resolve', async (req, res) => {
   const { q, m, r, t } = req.query;
 
   if (typeof (q) == 'string') {
-    const fn = r === 'true' ? nurlresolver.resolveRecursive : nurlresolver.resolve;
-    const result = await fn(q, {
-      extractMetaInformation: typeof (m) === 'string' && m === 'true',
-      timeout: typeof (t) == 'string' ? parseInt(t) : 8 || 8,  //8 seconds of timeout default
-      customResolvers: []
-    });
+    const result = await nurlresolver.resolve(q);
+    // const fn = r === 'true' ? nurlresolver.resolveRecursive : nurlresolver.resolve;
+    // const result = await fn(q, {
+    //   extractMetaInformation: typeof (m) === 'string' && m === 'true',
+    //   timeout: typeof (t) == 'string' ? parseInt(t) : 8 || 8,  //8 seconds of timeout default
+    //   customResolvers: []
+    // });
     res.json(result);
   } else {
     res.status(400).json({ error: 'Query param q not defined' });
