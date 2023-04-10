12ebdab2ed1daaab1292eef9ebc52883c898b57f
mnsrulz <manishrawat4u@gmail.com>
2023-04-10 03:01:01 +0000

    checking another

--- express/server.ts
+++ express/server.ts
@@ -7,10 +7,15 @@ const router = Router();
 import { nurlresolver } from 'nurlresolver';
 import cors from 'cors';
 
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+router.get('/', async (req, res) => {
+
+  const result = await nurlresolver.resolve('https://testing.com/t.m', {
+    extractMetaInformation: true,
+  });
+  res.json(result);
+  // res.writeHead(200, { 'Content-Type': 'text/html' });
+  // res.write('<h1>Hello from Express.js!</h1>');
+  // res.end();
 });
 
 router.get('/resolve', async (req, res) => {
